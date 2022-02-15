e95d8cc4952ce7d992912eb31e8e2bee4beca5ae
asinible <asinible@gitlab.com>
2022-02-15 20:56:16 +0600

    NEW

---
+++ courses/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import Subject, Module, Course
+from django.conf import settings
+from django.utils import translation
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class ModuleInline(admin.StackedInline):
+    model = Module
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'subject', 'created']
+    list_filter = ['created', 'subject']
+    search_fields = ['title', 'overview']
+    prepopulated_fields = {'slug': ('title',)}
+    inlines = [ModuleInline]
