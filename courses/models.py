142219e9beba48528b4d31080e7f692939cb28e3
asinible <asinible@gitlab.com>
2022-02-15 22:01:36 +0600

    orderfield

--- courses/models.py
+++ courses/models.py
@@ -1,10 +1,10 @@
-
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 from .fields import OrderField
+
 
 class Subject(models.Model):
     title = models.CharField(max_length=255)
@@ -46,9 +46,13 @@ class Module(models.Model):
                                on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=['course'])
 
     def __str__(self):
-        return self.title
+        return f'{self.order}. {self.title}'
+
+    class Meta:
+        ordering = ['order']
 
 
 class Content(models.Model):
@@ -58,7 +62,7 @@ class Content(models.Model):
                                )
     content_type = models.ForeignKey(ContentType,
                                      on_delete=models.CASCADE,
-                                     limit_choices_to={'model__in':(
+                                     limit_choices_to={'model__in': (
                                          'text',
                                          'video',
                                          'image',
@@ -66,6 +70,11 @@ class Content(models.Model):
                                      )})
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey('content_type', 'object_id')
+    order = OrderField(blank=True, for_fields=['module'])
+
+    class Meta:
+        ordering = ['order']
+
 
 class ItemBase(models.Model):
     owner = models.ForeignKey(User,
@@ -81,15 +90,18 @@ class ItemBase(models.Model):
     def __str__(self):
         return self.title
 
+
 class Text(ItemBase):
-    content  = models.TextField()
+    content = models.TextField()
+
 
 class File(ItemBase):
     file = models.FileField(upload_to='files')
 
+
 class Image(ItemBase):
     file = models.FileField(upload_to='images')
 
+
 class Video(ItemBase):
     url = models.URLField()
-
