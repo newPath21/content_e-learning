1e7f09e77d5b00d1e671e7e7e745ba9f8a3ffa16
asinible <asinible@gitlab.com>
2022-02-25 20:48:38 +0600

    mixins

--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,7 @@
+from pyexpat import model
+
 from django.urls import reverse_lazy
+from django.shortcuts import redirect, get_object_or_404
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, \
     DeleteView
@@ -9,11 +12,13 @@ from .models import Course
 
 from django.shortcuts import redirect, get_object_or_404
 from django.views.generic.base import TemplateResponseMixin, View
-from .forms import ModuleFormSet
+from django.db.models import Count
+
+from .models import Course, Module, Content, Subject
 
 from django.forms.models import modelform_factory
+from .forms import ModuleFormSet
 from django.apps import apps
-from .models import Module, Content
 
 
 class OwnerMixin(object):
@@ -157,7 +162,6 @@ class ModuleContentListView(TemplateResponseMixin, View):
                                    course__owner=request.user)
         return self.render_to_response({'module': module})
 
-from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 
 class ModuleOrderView(CsrfExemptMixin,
                       JsonRequestResponseMixin,
@@ -168,14 +172,34 @@ class ModuleOrderView(CsrfExemptMixin,
                                   course__owner=request.user).update(order=order)
         return self.render_json_response({'saved': 'OK'})
 
+
 class ContentOrderView(CsrfExemptMixin,
                        JsonRequestResponseMixin,
                        View):
     def post(self, request):
         for id, order in self.request_json.items():
             Content.objects.filter(id=id,
-                                   module__course__owner=request.user)\
-                                   .update(order=order)
+                                   module__course__owner=request.user) \
+                .update(order=order)
         return self.render_json_response({'saved': 'OK'})
 
 
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = 'courses/course/list.html'
+
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(total_courses=Count('courses'))
+        courses = Course.objects.annotate(total_modules=Count('modules'))
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+        return self.render_to_response({'subjects': subjects,
+                                        'subject': subject,
+                                        'courses': courses})
+
+from django.views.generic.detail import DetailView
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'courses/course/detail.html'
